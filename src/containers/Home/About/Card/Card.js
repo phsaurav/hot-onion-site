7abd6b04691ca88b76ba76d37e67d31a31b20574
PH Saurav <phsaurav29@gmail.com>
2021-10-16 15:05:15 +0600

    Alt added ♿

--- src/containers/Home/About/Card/Card.js
+++ src/containers/Home/About/Card/Card.js
@@ -7,6 +7,7 @@ const Card = ({ children, img }) => {
 		<div>
 			<div className="overflow-hidden h-auto w-80 lg:w-80 cursor-pointer mx-5 lg:mx-12 bg-white transition duration-500 ease-in-out transform hover:shadow-2xl my-5 rounded-2xl ">
 				<img
+					alt="Card Images"
 					src={img}
 					className="h-80 transition-height duration-500 ease-in-out hover:h-96 w-full object-cover rounded-2xl"
 				/>
